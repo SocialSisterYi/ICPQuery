50e927d726bdec95a59687d53149d497a7959998
SocialSisterYi <1440239038@qq.com>
2026-01-19 00:49:46 +0800

    更新http头部和超时

--- icpquery/dto.py
+++ icpquery/dto.py
@@ -36,12 +36,15 @@ class AsyncIcpQueryDto:
     ) -> None:
         self.client = httpx.AsyncClient(
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0",
                 "Referer": "https://beian.miit.gov.cn/",
                 "Origin": "https://beian.miit.gov.cn",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
             },
             follow_redirects=True,
             base_url=API_BASE,
+            timeout=20,
         )
         self.token = token
         self.refresh = refresh
